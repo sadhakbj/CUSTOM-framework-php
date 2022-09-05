d36aed4446c48c3552ad68868b8decd6280401b8
Bijaya Prasad Kuikel <sadhakbj@gmail.com>
2022-09-05 14:13:22 +0900

    Added unit tests with coverage report with xdebug.

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 namespace App\Core;
 
 use Symfony\Component\HttpFoundation\{Request, Response};
-use Symfony\Component\HttpKernel\Controller\{ArgumentResolver, ControllerResolver};
+use Symfony\Component\HttpKernel\Controller\{ArgumentResolverInterface, ControllerResolverInterface};
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 class Application
 {
     public function __construct(
-        private readonly UrlMatcher $matcher,
-        private readonly ControllerResolver $controllerResolver,
-        private readonly ArgumentResolver $argumentResolver
+        private readonly UrlMatcherInterface $matcher,
+        private readonly ControllerResolverInterface $controllerResolver,
+        private readonly ArgumentResolverInterface $argumentResolver
     ) {
     }
 
@@ -28,7 +28,9 @@ class Application
 
             return call_user_func_array($controller, $arguments);
         } catch (ResourceNotFoundException $exception) {
-            return new Response('Not Found', 404);
+            return new Response('Not Found', Response::HTTP_NOT_FOUND);
+        } catch (\Exception $exception) {
+            return new Response('Internal server error', Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -2,12 +2,63 @@
 
 namespace Tests;
 
+use App\Core\Application;
+use App\Http\Controllers\HelloController;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 
 class ApplicationTest extends TestCase
 {
-    public function testOk()
+    public function testNotFoundHandling()
     {
-        $this->assertTrue(true);
+        $framework = $this->getApplicationForException(new ResourceNotFoundException());
+        $response  = $framework->handle(new Request());
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+    }
+
+    public function testErrorHandling()
+    {
+        $framework = $this->getApplicationForException(new \RuntimeException());
+        $response  = $framework->handle(new Request());
+
+        $this->assertEquals(500, $response->getStatusCode());
+    }
+
+    public function testControllerResponse()
+    {
+        $matcher   = $this->createMock(UrlMatcherInterface::class);
+        $returnVal = [
+            '_route'      => 'hello/{name}',
+            'name'        => 'World',
+            '_controller' => [new HelloController(), 'index'],
+        ];
+        $matcher->expects($this->once())->method('match')->willReturn($returnVal);
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver   = new ArgumentResolver();
+
+        $framework = new Application($matcher, $controllerResolver, $argumentResolver);
+
+        $response = $framework->handle(new Request());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('Hello World', $response->getContent());
+    }
+
+    private function getApplicationForException($exception)
+    {
+        $matcher = $this->createMock(UrlMatcherInterface::class);
+        $matcher->expects($this->once())->method('match')->will($this->throwException($exception));
+        $controllerResolver = $this->createMock(ControllerResolverInterface::class);
+        $argumentResolver   = $this->createMock(ArgumentResolverInterface::class);
+
+        return new Application($matcher, $controllerResolver, $argumentResolver);
     }
 }
