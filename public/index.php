b4e153d82ffbe857a35241dc611845f00f790e62
Bijaya Prasad Kuikel <sadhakbj@gmail.com>
2023-11-05 21:18:01 +0900

    Fix the router & re-organize folders

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once  dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
 use Core\Application;
+use Core\Router;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\{Request};
 use Symfony\Component\HttpKernel\Controller\{ArgumentResolver, ControllerResolver};
@@ -11,11 +12,10 @@ use Symfony\Component\Routing\{Matcher\UrlMatcher, RequestContext};
 Debug::enable();
 
 
-$routes = require_once __DIR__ . '/../src/routes.php';
-
-$request            = Request::createFromGlobals();
-$context            = new RequestContext(method: $request->getMethod());
-$matcher            = new UrlMatcher($routes, $context);
+$request = Request::createFromGlobals();
+$context = new RequestContext(method: $request->getMethod());
+Router::initializeRoutes();
+$matcher            = new UrlMatcher(Router::loadRoutes(), $context);
 $controllerResolver = new ControllerResolver();
 $argumentResolver   = new ArgumentResolver();
 
